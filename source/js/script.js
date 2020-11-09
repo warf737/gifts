86ed9077ddbd2154d1d7d3bfd8b3e1158434218f
dvasilchuk <dvasilchuk@phoenixit.ru>
2020-11-09 21:12:55 +0300

    some refactoring

--- source/js/script.js
+++ source/js/script.js
@@ -1,34 +1,70 @@
 window.onload = function() {
   const merrywrap = document.getElementById(`merrywrap`);
   const boxes = merrywrap.querySelectorAll(`.giftbox`);
+  let activeBox;
 
+  /**
+   * Добавляем каждой коробке обработчик события наведения курсора
+   * При наведении курсора на коробку добавляем ей обработчик клика на эту коробку и
+   * обработчик события ухода курсора с этой коробки
+   */
   const init = () => {
-    // alert(`${document.documentElement.clientWidth}x${document.documentElement.clientHeight}`);
-    boxes.forEach(function(box) {
-      const activeAreas = box.querySelectorAll(`.active-area`);
-      activeAreas.forEach(function(area) {
-        area.addEventListener(`mouseover`, function(evt) {
-          addShake(evt, box);
-        });
+    boxes.forEach((box) => {
+      box.addEventListener(`mouseover`, function _handler() {
+          console.log(activeBox);
+          console.log('2');
+          activeBox = box;
+          addShake(box);
+
+        box.addEventListener(`click`, function() {
+          openBox(box)
+        }, true);
+
+      },true);
+
+    });
+  };
+
+  /**
+   * Добавляет классы, отвечающие за визуальное отображение коробки на короторой находится курсор
+   * @param evt
+   * @param box
+   */
+  const addShake = (box) => {
+    if (box) {
+      // box.classList.add(`shake`, `shadow`, `scaled`);
+      box.classList.add(`shadow`);
+      box.addEventListener(`mouseout`, () => {
+        removeShake(box)
       });
-    });
+      // box.addEventListener(`click`, function(evt) {
+      //   openBox(evt, box);
+      // })
+    };
   };
 
-
-  const addShake = function (evt, box) {
-    const area = evt.target;
-    if (box) { box.classList.add(`shake`, `shadow`, `scaled`); }
-    area.removeEventListener(`mouseover`, addShake);
-    area.addEventListener(`mouseout`, function(){
-      removeShake(evt, box)
-    });
+  /**
+   * Убирает классы, отвечающие за визуальное отображение коробки на короторой находится курсор
+   * @param evt
+   * @param box
+   */
+  const removeShake = (box) => {
+    if (box) {
+      // activeBox = '';
+      box.classList.remove(`shake`, `shadow`, `scaled`);
+      // box.removeEventListener(`click`, openBox);
+    }
   };
 
-  const removeShake = function (evt, box) {
-    const area = evt.target;
-    if(box) { box.classList.remove(`shake`, `shadow`, `scaled`); }
-    area.removeEventListener(`mouseout`, removeShake);
-    area.addEventListener(`mouseover`, addShake);
+  /**
+   * Добавляет классы, отвечающие за события при открытии коробки
+   * @param evt
+   * @param box
+   */
+  const openBox = (box) => {
+    // box.removeEventListener(`click`, openBox);
+    console.log('event: ', box);
+    // console.log('opened a box ', box);
   };
 
   init();
